f8aace2094aa7305d5589fd7f01a32e5ba0074b8
Ivylucyawuor <awuorivy09>
2024-09-01 13:31:14 +0300

    OOP

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+     def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count 
+
+        if not isinstance(title, str):
+            raise ValueError("Title must be a string")
+        if not isinstance(page_count, int):
+            raise ValueError("page_count must be an integer")
+
+     @property
+     def page_count(self):
+        return self._page_count
+
+     @page_count.setter
+     def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            return
+        self._page_count = value
+
+     def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+        if not isinstance(brand, str):
+            raise ValueError("Brand must be a string")
+        if not isinstance(size, int):
+            raise ValueError("Size must be an integer")
+        
+         
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+        if not isinstance(value, str):
+            raise ValueError("Brand must be a string")
+        
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
+        if not isinstance(value, int):
+            print("size must be an integer") 
+            return
+        
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
